662042c64551731db238f656be94db0e5e74463a
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-10 19:11:57 +0100

    2

--- js/ej2.js
+++ js/ej2.js
@@ -1,9 +1,13 @@
 $(function() {
-    $('.b1').click(function() { $('.caja').slideUp(300) });
-    $('.b2').click(function() { $('.b2').hide() })
-    $('.b3').click(function() { $('.b3').fadeOut(500) })
-    $('.b5').click(function() { $('.b5').css("color", "yellow") })
-    $('.b6').click(function() { $('.b6').css("color", "orange") })
-    $('.b7').click(function() { $('.b7').html("pulsado") })
+    $('#b1').click(function() { $('.caja').slideUp(300) });
+    $('#b2').click(function() { $('.caja').slideDown(300) })
+    $('#b3').click(function() { $('.caja').slideToggle(300) })
+    $('#b5').bind("click mouseenter", function() { $('.caja').css("background-color", "green") })
+    $('#b6').click(function() {
+        $(".caja").fadeIn(100);
+        $(".caja").delay(800);
+        $(".caja").fadeOut(800);
+    })
+    $('#b7').click(function() { $('.caja').toggleClass("animacion") })
 
 })
